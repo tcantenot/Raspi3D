fdd63782098f03a8da4d8a215565c2aeca1f2c93
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2013-12-10 10:21:12 +0100

    WIP

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -17,6 +17,28 @@ inline double findnoise2(double x,double y)
     return 1.0-((double)nn/1073741824.0);
 }
 
+inline double interpolate(double a,double b,double x)
+{
+    double ft = x * 3.1415927;
+    double f = (1.0 - std::cos(ft))* 0.5;
+    return a*(1.0-f)+b*f;
+}
+
+inline double noise(double x, double y)
+{
+    double floorx = (double) ((int) x);
+    double floory = (double) ((int) y);
+
+    double s,t,u,v;//Integer declaration
+    s=findnoise2(floorx,floory);
+    t=findnoise2(floorx+1,floory);
+    u=findnoise2(floorx,floory+1);//Get the surrounding pixels to calculate the transition.
+    v=findnoise2(floorx+1,floory+1);
+    double int1=interpolate(s,t,x-floorx);//Interpolate between the values.
+    double int2=interpolate(u,v,x-floorx);//Here we use x-floorx, to get 1st dimension. Don't mind the x-floorx thingie, it's part of the cosine formula.
+    return interpolate(int1,int2,y-floory);//Here we use y-floory, to get the 2nd dimension.
+}
+
 }
 
 Terrain::Terrain(Size w, Size h):
@@ -46,10 +68,10 @@ Terrain::Terrain(Size w, Size h):
     {
         for(Size j = 0; j < h; ++j)
         {
-            vertices.push_back(i)  ; vertices.push_back(findnoise2(i, j)    ); vertices.push_back(j)  ;
-            vertices.push_back(i)  ; vertices.push_back(findnoise2(i, j+1)  ); vertices.push_back(j+1);
-            vertices.push_back(i+1); vertices.push_back(findnoise2(i+1, j)  ); vertices.push_back(j)  ;
-            vertices.push_back(i+1); vertices.push_back(findnoise2(i+1, j+1)); vertices.push_back(j+1);
+            vertices.push_back(i)  ; vertices.push_back(noise(i, j)    ); vertices.push_back(j)  ;
+            vertices.push_back(i)  ; vertices.push_back(noise(i, j+1)  ); vertices.push_back(j+1);
+            vertices.push_back(i+1); vertices.push_back(noise(i+1, j)  ); vertices.push_back(j)  ;
+            vertices.push_back(i+1); vertices.push_back(noise(i+1, j+1)); vertices.push_back(j+1);
         }
     }
 
