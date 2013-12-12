f42fdec6ff6b2f64e2a6da4292056f1e5569eb3d
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2013-12-12 21:10:18 +0100

    Smooth heightmap - WIP

--- src/Window.cpp
+++ src/Window.cpp
@@ -545,7 +545,7 @@ void Window::grabMousePointer(bool grab) const
 
 void Window::init() const
 {
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glEnable(GL_DEPTH_TEST);
 }
 
