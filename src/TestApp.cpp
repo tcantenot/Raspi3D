3667925b8604bd1164f7a2d6c75b0e3bce56ed7d
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2013-12-12 13:42:58 +0100

    Smooth heightmap - WIP

--- src/TestApp.cpp
+++ src/TestApp.cpp
@@ -142,7 +142,7 @@ void TestApp::run()
 
             if(std::fabs(timeWave) < std::numeric_limits<float>::epsilon())
             {
-                timeWave = 0.f;
+                timeWave = timeWaveStep;
             }
 
             //std::cout << timeWave << std::endl;
