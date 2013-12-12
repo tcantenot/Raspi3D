eada69ca74253e1fb876902a790f69f496a736c4
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2013-12-12 21:21:01 +0100

    Smooth heightmap - WIP

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ void parse_args(int argc, char ** argv);
 
 int main(int argc, char ** argv)
 {
-    //parse_args(argc, argv);
+    parse_args(argc, argv);
 
     //Scheduler::SetScheduler(getpid(), SchedulerType::RoundRobin, 85);
 
